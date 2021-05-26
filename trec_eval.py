0876c0bf612e302a7a9258acf447056c45279b23
Lingxiao1 <32996976+Lingxiao1@users.noreply.github.com>
2021-05-26 19:14:23 +0900

    Delete trec_eval.py

--- trec_eval.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def trec_eval(qrel_file,trec_file):
-    command = ['./trec_eval',qrel_file,trec_file]
-    output = subprocess.Popen(command,stdout=subprocess.PIPE).communicate()
-    return output[0]
